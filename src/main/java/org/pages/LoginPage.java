60dc6b66629feec6275ff6074d412e9462250bc0
swappyyyy <hswapnil2293>
2022-03-25 13:57:14 +0530

    second commit

--- src/main/java/org/pages/LoginPage.java
+++ src/main/java/org/pages/LoginPage.java
@@ -1,5 +1,10 @@
 package org.pages;
 
 public class LoginPage {
+	public void loginPage() {
+		System.out.println("this is login page");
+		System.out.println("this is NEW FIRST COMMIT");
+
+	}
 
 }
